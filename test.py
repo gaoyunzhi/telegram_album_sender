ab53da26b78eea16367091dba199f119f2342803
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-17 05:55:09 -0700

    commit

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 import album_sender
 import web_2_album
 import yaml
+from telegram_util import AlbumResult as Result
 from telegram.ext import Updater
 
 with open('CREDENTIALS') as f:
@@ -16,8 +17,11 @@ def test(url):
 	album_sender.send(chat, url, result)	
 	
 if __name__=='__main__':
-	test('https://www.douban.com/people/46037668/status/2956088549/')
+	# test('https://www.douban.com/people/46037668/status/2956088549/')
 	# test('https://www.douban.com/people/kinokinokino/status/2947364315/')
+	result = Result()
+	result.imgs = ['https://mmbiz.qpic.cn/mmbiz_png/e6dIc6Q5sLoTzby70dnXeV3QOV35Jbak8KJ0ibleGbX6cJlySvc4A3Wb5Sh4qbicN1brKo2expHd6dVGebsZgKHA/640?wx_fmt=png', 'https://mmbiz.qpic.cn/mmbiz_png/e6dIc6Q5sLoAFc3BPqsTicic55loUJXibLZj8zv7TAia0EYa4tYAr1qRjRcKnYMfc9y2TibHJpZdAKORxQBqqcPa2jg/640?wx_fmt=png']
+	album_sender.send(chat, '', result)
 	
 
 # TODO: rename the module to album_sender?
