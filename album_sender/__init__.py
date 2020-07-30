ef3011bc6c4c9bdd72b861bc46449b70985da1b3
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-29 19:48:57 -0700

    commit

--- album_sender/__init__.py
+++ album_sender/__init__.py
@@ -38,44 +38,58 @@ def shouldSendAnimation(result):
 	result.imgs = [animated_imgs[-1][1]]
 	return True
 
-def send(chat, url, result, rotate=0):
-	suffix = '[source](%s)' % url
+def getCap(result, limit):
+	suffix = '[source](%s)' % result.url
+	return cutCaption(result.cap, suffix, limit)
 
+def sendVideo(chat, result):
+	os.system('mkdir tmp > /dev/null 2>&1')
+	with open('tmp/video.mp4', 'wb') as f:
+		f.write(cached_url.get(result.video, force_cache=True, mode='b'))
+	if os.stat('tmp/video.mp4').st_size > 50 * 1024 * 1024:
+		return []
+	group = [InputMediaVideo(open('tmp/video.mp4', 'rb'), 
+		caption=getCap(result, 1000), parse_mode='Markdown')]
+	return chat.bot.send_media_group(chat.id, group, timeout = 20*60)
+
+def imgRotate(img_path, rotate):
+	if not rotate:
+		return
+	if rotate == True:
+	img = Image.open(img_path)
+	img = img.rotate(rotate, expand=True)
+	img.save(img_path)
+
+def send_v2(chat, result, rotate=0, send_all=False):
 	if result.video:
-		os.system('mkdir tmp > /dev/null 2>&1')
-		with open('tmp/video.mp4', 'wb') as f:
-			f.write(cached_url.get(result.video, force_cache=True, mode='b'))
-		if os.stat('tmp/video.mp4').st_size > 50 * 1024 * 1024:
-			return
-		group = [InputMediaVideo(open('tmp/video.mp4', 'rb'), 
-			caption=cutCaption(result.cap, suffix, 1000), parse_mode='Markdown')]
-		return chat.bot.send_media_group(chat.id, group, timeout = 20*60)
-
-	cap = cutCaption(result.cap, suffix, 1000)
+		return sendVideo(chat, result)
+		
 	if shouldSendAnimation(result):
 		return chat.bot.send_document(chat.id, 
 			open(cached_url.getFilePath(result.imgs[0]), 'rb'), 
-			caption=cap, parse_mode='Markdown', timeout = 20*60)
-		
-	imgs = pic_cut.getCutImages(result.imgs, 10)	
+			caption=getCap(result, 1000), parse_mode='Markdown', 
+			timeout=20*60)
+
+	img_limit = 100 if send_all else 10
+	imgs = pic_cut.getCutImages(result.imgs, img_limit)	
 	imgs = [x for x in imgs if properSize(x)]
+	[imgRotate(x, rotate) for x in imgs]
+	print(len(imgs))
 	if imgs:
-		if rotate:
-			if rotate == True:
-				rotate = 180
-			for img_path in imgs:
-				img = Image.open(img_path)
-				img = img.rotate(rotate, expand=True)
-				img.save(img_path)
-		group = [InputMediaPhoto(open(imgs[0], 'rb'), 
-			caption=cap, parse_mode='Markdown')] + \
-			[InputMediaPhoto(open(x, 'rb')) for x in imgs[1:]]
-		return chat.bot.send_media_group(chat.id, group, timeout = 20*60)
+		result = []
+		for page in range(1 + int((len(imgs) - 1) / 10)):
+			group = ([InputMediaPhoto(open(imgs[page * 10], 'rb'), 
+				caption=getCap(result, 1000), parse_mode='Markdown')] + 
+				[InputMediaPhoto(open(x, 'rb')) for x in 
+					imgs[page * 10 + 1:page * 10 + 11]])
+			result += chat.bot.send_media_group(chat.id, group, timeout = 20*60)
+		return result
 
 	if result.cap:
-		return [chat.send_message(cutCaption(result.cap, suffix, 4000), 
+		return [chat.send_message(getCap(result, 4000), 
 			parse_mode='Markdown', timeout = 20*60, 
 			disable_web_page_preview = (not isUrl(result.cap)))]
 
-def send_v2(chat, result, rotate=0):
-	return send(chat, result.url, result, rotate=rotate)
+def send(chat, url, result, rotate=0, send_all=False):
+	result.url = url
+	send_v2(chat, result, rotate=rotate, send_all=send_all)
--- test.py
+++ test.py
@@ -12,13 +12,14 @@ with open('CREDENTIALS') as f:
 	CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
 tele = Updater(CREDENTIALS['bot_token'], use_context=True)
 chat = tele.bot.get_chat(-1001198682178)
+# chat = tele.bot.get_chat('@web_record')
 
 def test(url):
 	result = weibo_2_album.get(url)
-	album_sender.send_v2(chat, result)	
+	album_sender.send_v2(chat, result, send_all=True)	
 	
 if __name__=='__main__':
-	test('https://m.weibo.cn/detail/4529592491319532#comment')
+	test('https://movie.douban.com/subject/27010768/photos?type=R&start=0&sortby=like&size=a&subtype=a')
 	
 
 # TODO: rename the module to album_sender?
