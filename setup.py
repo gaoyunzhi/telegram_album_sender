38ea0fead8d178b718f4785e300249feb9de1c4e
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-24 12:06:00 -0800

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="album_sender",
-    version="0.0.28",
+    version="0.0.29",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Telegram album sender.",
